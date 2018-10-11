97352c5b45f69b5b648f8e78b71b1bb39bc83262
Patricksirich <Patricksirich@hotmail.com>
2018-10-11 10:50:43 +0200

    Mandatory1

---
+++ src/TCPClient.java
@@ -0,0 +1,149 @@
+import java.net.*;
+import java.util.*;
+import java.io.*;
+
+public class TCPClient {
+
+    static Thread msgFromServer;
+    static Thread IMAV;
+    static OutputStream outToServer;
+    static InputStream inFromServer;
+    static String USERNAME;
+
+    public static void main(String[] args) {
+        System.out.println("=============CLIENT==============");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("What is your username: ");
+
+
+        do {
+            // tjek om username overholder protokollen. Loop hvis ikke.
+            String userName = args.length >= 1 ? args[0] : sc.nextLine();
+            if ((userName.matches("^[a-zA-Z\\d-_]{0,12}$"))) {
+                USERNAME = userName;
+                break;
+            }
+            System.out.println("Username is too long. Max 12 digits or letters and only '_', '-' is allowed. \n Please pick a new username");
+        } while (true);
+
+        System.out.print("What is the IP for the server (type 0 for localhost): ");
+        String ipToConnect = args.length >= 2 ? args[1] : sc.nextLine();
+
+        System.out.print("What is the PORT for the server: ");
+        int portToConnect = args.length >= 3 ? Integer.parseInt(args[2]) : sc.nextInt();
+
+
+        final int PORT_SERVER = portToConnect;
+        final String IP_SERVER_STR = ipToConnect.equals("0") ? "127.0.0.1" : ipToConnect;
+
+
+        try {
+
+
+            InetAddress ip = InetAddress.getByName(IP_SERVER_STR);
+            Socket socket = new Socket(ip, PORT_SERVER);
+
+
+            System.out.println("\nConnecting...");
+            System.out.println("Username: " + USERNAME);
+            System.out.println("SERVER IP: " + IP_SERVER_STR);
+            System.out.println("SERVER PORT: " + PORT_SERVER + "\n");
+
+            outToServer = socket.getOutputStream();
+            outToServer.write(("JOIN " + USERNAME + ", " + IP_SERVER_STR + ":" + portToConnect).getBytes());
+
+            inFromServer = socket.getInputStream();
+            byte[] acceptedClient = new byte[1024];
+            inFromServer.read(acceptedClient);
+            String msgAccepted = new String(acceptedClient);
+
+            //Tjek for duplicate username (ikke testet)
+            if (msgAccepted.equalsIgnoreCase("J_ER")) {
+                sc = new Scanner(System.in);
+                System.out.println("What is your new username: ");
+                USERNAME = sc.nextLine();
+                System.out.println("new username = " + USERNAME);
+                byte[] newUserName = USERNAME.getBytes();
+                outToServer.write(newUserName);
+            }
+
+            //start IMAV og receieMsg.
+            imavThread();
+            receiveMsg();
+
+
+            //tillad at man kan sende beskeder uafhængigt af at modtage beskeder.
+            while (true) {
+
+
+                outToServer = socket.getOutputStream();
+
+                sc = new Scanner(System.in);
+
+                do {
+                    System.out.println("\nWhat do you want to send? ");
+                    String msgToSend = "DATA " + USERNAME + ": " + sc.nextLine();
+                    byte[] dataToSend = msgToSend.getBytes();
+
+                    if (msgToSend.equalsIgnoreCase("DATA " + USERNAME + ": " + "!quit")) {
+                        msgFromServer.stop();
+                        IMAV.stop();
+                        System.out.println("Shutting down");
+                    }
+
+                    if (msgToSend.trim().length() < 250) {
+                        outToServer.write(dataToSend);
+                        break;
+                    }
+                    System.out.println("Your message length is: " + msgToSend.trim().length() + " which is " + (msgToSend.trim().length() - 250) + " too long, your message can be no longer than 250 characters long.");
+                } while (true);
+
+
+            }
+
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // modtag beskeder fra serveren uafhængig af at sende beskeder.
+    static void receiveMsg() {
+        msgFromServer = new Thread(() -> {
+            try {
+                while (true) {
+
+                    byte[] dataIn = new byte[1024];
+                    inFromServer.read(dataIn);
+                    String msgIn = new String(dataIn);
+                    msgIn = msgIn.trim();
+                    System.out.println(msgIn);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        msgFromServer.start();
+    }
+
+    //start IMAV
+    static void imavThread() {
+        IMAV = new Thread(() -> {
+            while (true) {
+                try {
+                    Thread.sleep(40000);
+                    outToServer.write("IMAV".getBytes());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        IMAV.start();
+    }
+
+}
---
+++ src/TCPServer.java
@@ -0,0 +1,148 @@
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+
+public class TCPServer {
+
+    static Thread t;
+    static InputStream input;
+    static OutputStream output;
+    static Socket socket;
+
+    public static void main(String[] args) {
+        System.out.println("=============SERVER==============");
+
+        final int PORT_LISTEN = 5656;
+
+        ArrayList<Client> users = new ArrayList<>();
+
+
+        try {
+
+            ServerSocket server = new ServerSocket(PORT_LISTEN);
+            System.out.println("Server starting...\n");
+
+
+            while (true) {
+                Client client = new Client();
+                socket = server.accept();
+                System.out.println("Client connected");
+// Client thread
+                t = new Thread(() -> {
+
+                    InputStream userName = null;
+                    try {
+                        userName = socket.getInputStream();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    byte[] uName = new byte[1024];
+                    try {
+                        userName.read(uName);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    String USERNAME = new String(uName);
+                    USERNAME = USERNAME.substring(5, USERNAME.lastIndexOf(","));
+
+
+                    String clientIp = socket.getInetAddress().getHostAddress();
+                    System.out.println(USERNAME);
+                    System.out.println("IP: " + clientIp);
+                    System.out.println("PORT: " + socket.getPort());
+
+                    OutputStream acceptSocket = null;
+                    try {
+                        acceptSocket = socket.getOutputStream();
+                        input = socket.getInputStream();
+                        output = socket.getOutputStream();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    client.setName(USERNAME);
+                    client.setSocket(socket);
+                    client.setOutput(output);
+                    client.setInput(input);
+
+                    //Tilføjer client til users ArrayList
+                    users.add(client);
+                    System.out.println(client);
+                    String clientAccept = "J_OK";
+                    System.out.println(users);
+                    System.out.println("JOIN " + USERNAME + ", " + clientIp + ":" + PORT_LISTEN);
+                    byte[] acceptSend = clientAccept.getBytes();
+                    try {
+                        acceptSocket.write(acceptSend);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+
+                    try {
+                        ListOfClients(users);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+
+                    //modtag beskeder
+                    do {
+                        try {
+                            byte[] dataIn = new byte[1024];
+                            input.read(dataIn);
+                            String msgIn = new String(dataIn);
+                            msgIn = msgIn.trim();
+                            System.out.println(msgIn);
+                            if (msgIn.equalsIgnoreCase("DATA " + USERNAME + ": !quit")) {
+                                client.getSocket().close();
+                                users.remove(client);
+                                ListOfClients(users);
+                                System.out.println(users);
+                            }
+                            //Tjek om beskeden er over 250 karaktere, send error tilbage hvis sandt.
+                            if (msgIn.trim().length() > 250) {
+                                byte[] J_ER_TooLong;
+                                String J_ER_long = "J_ER MESSAGE TOO LONG: Message contains: " + msgIn.length() + " characters, max length is 250";
+                                J_ER_TooLong = J_ER_long.getBytes();
+                                output.write(J_ER_TooLong);
+                            } else {
+                                //send beskeden til alle brugere.
+                                for (Client c : users) {
+                                    if (!msgIn.equals("IMAV") && !msgIn.equalsIgnoreCase("DATA " + USERNAME + ": !quit")) {
+                                        output = c.getOutput();
+                                        output.write(dataIn);
+                                    }
+                                }
+                            }
+//                                }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    } while (!socket.isClosed());
+
+//                    }
+                });
+                t.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //metode som sender en liste af alle aktive klienter ud til alle aktive brugere. Bliver kaldt ved ændring.
+    static void ListOfClients(ArrayList<Client> users) throws IOException {
+
+        String clientList = "";
+        for (Client c : users) {
+            clientList += c.getName() + ", ";
+        }
+        for (Client c : users) {
+            output = c.getOutput();
+            String cList = "List of all clients: " + clientList.substring(0, clientList.lastIndexOf(","));
+            byte[] listofclients;
+            listofclients = cList.getBytes();
+            output.write(listofclients);
+        }
+    }
+
+}
